dd115182074fefe0d51e3216cf4d11ee9eb17edc
Adit <adit.mahmood1@gmail.com>
2020-08-01 11:39:10 -0400

    adding PlotlyHW

--- PlotlyHW/static/js/Plotly_App.js
+++ PlotlyHW/static/js/Plotly_App.js
@@ -1,15 +1,15 @@
-d3.json("/data/samples.json").then(function(ChartData) {
-  var names = ChartData.names;
-  console.log(names);
-  var dropDownButton = d3.select("#selDataset");
-    names.forEach((Case_Number) => {
-      var row = dropDownButton.append("option");
-      row.text(Case_Number);
-      row.attr("value", "100");
-      row.property("value", "101");
+function buildData(dropDownData) {
+  d3.json("/data/samples.json").then((data) => {
+    var Data = data.metadata;
+    var resultArray = Data.filter(dropDownDataObj => dropDownDataObj.id == dropDownData);
+    var result = resultArray[0];
+    var panelData = d3.select("#sample-metadata");
+    panelData.html("");
+    Object.entries(result).forEach(([key, value]) => {
+      panelData.append("h6").text(`${key.toUpperCase()}: ${value}`);
     });
-})
-
+  });
+}
 function buildGraph(dropDown) {
   console.log(dropDown);
   d3.json("/data/samples.json").then(function(importedData) {
@@ -22,61 +22,74 @@ function buildGraph(dropDown) {
     var sample_values = selectedSamples.sample_values;
     var ten_otu_ids = selectedSamples.otu_ids;
     var otu_labels = selectedSamples.otu_labels;
-    console.log(ten_otu_ids);
 
-    var Ten_Sample_Values = sample_values.sort((a, b) => b - a);
-    var top_ten_sv = Ten_Sample_Values.slice(0, 10);
-    console.log(top_ten_sv);
-
-    var Ten_otu_ids = ten_otu_ids.sort((c, d) => d - c);
-    var top_ten_otu_ids = Ten_otu_ids.slice(0, 10);
-    console.log(top_ten_otu_ids);
-
-    var Ten_otu_labels = otu_labels.sort((c, d) => d - c);
-    var top_ten_otu_labels = Ten_otu_labels.slice(0, 10);
-    console.log(top_ten_otu_labels);
-
-    var demographic_id = importedData.metadata[0];
-    var demographic_ethnicity = importedData.metadata[1];
-    var demographic_gender = importedData.metadata[2];
-    var demographic_age = importedData.metadata[3];
-    var demogrpahic_location = importedData.metadata[4];
-    var demogrpahic_bbtype = importedData.metadata[5];
-    var demographic_wfreq = importedData.metadata[6];
-    
-    var bar_plot_trace = {
-      type: "bar",
-      x: top_ten_sv,
-      y: top_ten_otu_ids,
-      orientation: "h",
-      text: top_ten_otu_labels
+    var BubbleLayout = {
+      title: "Bacteria Samples Per Sample",
+      margin: { t: 0 },
+      hovermode: "closest",
+      xaxis: { title: "OTU ID"},
+      margin: { t: 30}
     };
 
-    var layout2  = {
-      title: "Bubble Plot"
-    }
-
-    var bubble_plot_trace = [{
-      x: top_ten_sv,
-      y: top_ten_otu_ids,
-      type: "bubble",
-      text: top_ten_otu_labels,
-      marker: {
-        size: top_ten_sv,
-        color: top_ten_otu_ids
+    var BubbleData = [
+      {
+        x: ten_otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: "markers",
+        marker: {
+          size: sample_values, 
+          color: ten_otu_ids,
+          colorscale: "Earth"
+        }
       }
-    }]
+    ]
 
-    var data = [bar_plot_trace];
+    Plotly.newPlot("bubble", BubbleData, BubbleLayout);
 
-    var layout1 = {
-      title: "Bar Plot", 
+    var YTicks = ten_otu_ids.slice(0, 10).map(TenOtuIds => `OTU ${TenOtuIds}`).reverse();
+    var BarData = [
+      {
+        y: YTicks,
+        x: sample_values.slice(0,10).reverse(),
+        text: otu_labels.slice(0, 10).reverse(),
+        type: "bar", 
+        orientation: "h",
+      }
+    ];
+
+    var BarLayout = {
+      title: "Top Ten Bacteria Found",
+      margin: { t: 30, l: 150}
     };
 
-    Plotly.newPlot("bar", data, layout1);
-    Plotly.newPlot("bubble", bubble_plot_trace, layout2);
-  })
-
+    Plotly.newPlot("bar", BarData, BarLayout);
+  });  
 }
 
-buildGraph()
+function init() {
+  var Selector = d3.select("#selDataset");
+  d3.json("/data/samples.json").then(function(ChartData) {
+    var names = ChartData.names;
+    console.log(names);
+    var dropDownButton = d3.select("#selDataset");
+      names.forEach((Case_Number) => {
+        Selector
+          .append("option")
+          .text(Case_Number)
+          .property("value", Case_Number);
+      });
+
+      var firstSample = ChartData[0];
+      buildGraph(firstSample);
+      buildData(firstSample);
+  });
+}
+
+function optionChanged(newSample) {
+  buildGraph(newSample);
+  buildData(newSample);
+}
+
+init();
+
